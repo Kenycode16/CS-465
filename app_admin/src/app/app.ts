a4c951c55b632b77060c6478d3f38e9667138594
Aziz Zaghdoudi <aziz@aims.team>
2025-10-12 01:07:38 -0400

    init module6 + adding add Trip button anf function

---
+++ app_admin/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { TripListingComponent } from './trip-listing/trip-listing';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule,RouterOutlet,TripListingComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'Welcome to Tropical Trips Admin'
+}
