281fa37277e876ab1c6c776656a072ae220181d3
Aziz Zaghdoudi <aziz@aims.team>
2025-10-16 00:26:04 -0400

    no chnages

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,25 +1,15 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-=======
 const jwt = require('jsonwebtoken');
->>>>>>> origin/module7
 
 
 
 const tripsController = require('../controllers/trips');
 const authController = require('../controllers/authentication');
-<<<<<<< HEAD
-
-
-router.route('/register').post(authController.register);
-//router.route('/login').post(authController.login);
-=======
 const authenticateJWT = require('../config/jwt');
 
 router.route('/register').post(authController.register);
 router.route('/login').post(authController.login);
->>>>>>> origin/module7
 
 //define route for our trips endpoint 
 router
@@ -29,7 +19,6 @@ router
     .post(tripsController.tripsAddTrip); // Post method add a new trip
 =======
     .post(authenticateJWT,tripsController.tripsAddTrip); // Post method add a new trip
->>>>>>> origin/module7
 
 
 
@@ -38,11 +27,7 @@ router
 router
     .route("/trips/:tripCode")
     .get(tripsController.tripsFindByCode)
-<<<<<<< HEAD
-    .put(tripsController.tripsUpdateTrip); // Put method to update a trip
-=======
     .put(authenticateJWT,tripsController.tripsUpdateTrip); // Put method to update a trip
->>>>>>> origin/module7
 
 
 
