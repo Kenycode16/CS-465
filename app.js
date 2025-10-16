4fb7354e150f87b66b1b1f3e3c79e99ba3e9189e
Aziz Zaghdoudi <aziz@aims.team>
2025-10-16 01:00:26 -0400

    updates

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,29 +1,31 @@
 const express = require('express');
+
 const router = express.Router();
-
-
-
-
-
-
-
-
+//
 const tripsController = require('../controllers/trips');
 const authController = require('../controllers/authentication');
+const { authenticateJWT } = require('../config/jwt');
+
+const jwt = require('jsonwebtoken');
+//
 
 
 
+// define route for login endpoint
+router
+.route('/login')
+.post(authController.login);
 
-const authenticateJWT = require('../config/jwt');
-
-router.route('/register').post(authController.register);
-router.route('/login').post(authController.login);
+// define route for register endpoint
+router
+.route('/register')
+.post(authController.register);
 
 
 //define route for our trips endpoint 
 router
     .route("/trips")
-    
+
     .get(tripsController.tripsList)
 
     .post(authenticateJWT,tripsController.tripsAddTrip); // Post method add a new trip
@@ -35,7 +37,7 @@ router
 //GET method routes tripsFindByCode - require parameter
 router
     .route("/trips/:tripCode")
-    .get(tripsController.tripsFindByCode) // Put method to update a trip
+    .get(tripsController.tripsFindByCode)
     .put(authenticateJWT,tripsController.tripsUpdateTrip); // Put method to update a trip
 
 
